8e71d591f4ac3daafaa2cbcfca6007b63d3939fe
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-05 12:21:44 +0900

    断層色分け追加

--- Map-FaultDrawTest/Form1.cs
+++ Map-FaultDrawTest/Form1.cs
@@ -30,24 +30,91 @@ namespace Map_FaultDrawTest
             var latEnd = 50.0f;
             var lonSta = 120.0f;
             var lonEnd = 150.0f;
-            var zoom = 1080f / (latEnd - latSta);
+            var size = 1080f * 1;
+            var zoom = size / (latEnd - latSta);
 
-            var img = DrawMap(latSta, latEnd, lonSta, lonEnd);
+            var img = DrawMap(latSta, latEnd, lonSta, lonEnd, size);
             using var g = Graphics.FromImage(img);
 
-
+            //var faultCsv = new StringBuilder("id,name,name1,active,size,rank\n");
 
             foreach (var feature in fault.Features)
             {
+                // 活断層における今後30年以内の地震発生確率が3%以上を「Ｓランク」、0.1～3％未満を「Ａランク」、0.1%未満を「Ｚランク」、不明（すぐに地震が
+                // 起きることが否定できない）を「Ｘランク」と表記している。地震後経過率（注2）が0.7以上である活断層については、ランクに「＊」を付記してい
+                // る。Ｚランクでも、活断層が存在すること自体、当該地域で大きな地震が発生する可能性を示す。
+                /*
+                faultCsv.Append(feature.Properties.Id);
+                faultCsv.Append(',');
+                faultCsv.Append(feature.Properties.Name);
+                faultCsv.Append(',');
+                faultCsv.Append(feature.Properties.Name1);
+                faultCsv.Append(',');
+                faultCsv.Append(feature.Properties.Active);
+                faultCsv.Append(',');
+                faultCsv.Append(feature.Properties.Size);
+                faultCsv.Append(',');
+                faultCsv.Append(feature.Properties.Rank);
+                faultCsv.AppendLine();
+                */
+                var rankSt = feature.Properties.Rank;
+                var rankId = -1;
+                switch (rankSt.Split("として").Last().Replace('（', '(').Split('(')[0])//中南部としてA*ランク　くらいしかないけど　複数ケースは1を利用 なぜか全角半角かっこあるので統一
+                {
+                    //複数ケース例: https://www.jishin.go.jp/regional_seismicity/rs_katsudanso/f103_muikamachi/　https://www.jishin.go.jp/regional_seismicity/rs_katsudanso/shinji/
+                    case "Sランク":
+                    case "S*ランク":
+                        rankId = 1;
+                        break;
+                    case "Aランク":
+                    case "A*ランク":
+                        rankId = 2;
+                        break;
+                    case "Zランク":
+                        rankId = 3;
+                        break;
+                    case "Xランク":
+                        rankId = 4;
+                        break;
+                    case "-":
+                    case "単独で震源断層となることはないと推定":
+                        rankId = 5;
+                        break;
+                }
+                if (rankId == -1) Console.WriteLine("vvvvvvvvvv不明ランク検知vvvvvvvvvv");
+                Console.WriteLine(feature.Properties.Name + " : " + rankSt + " = " + rankSt.Split("として").Last().Replace('（', '(').Split('(')[0] + " => " + rankId);
+
                 foreach (var singleObject in feature.Geometry.Coordinates.Objects)
                 {
                     var points = singleObject.MainPoints.Select(coordinate => new PointF((coordinate.Lon - lonSta) * zoom, (latEnd - coordinate.Lat) * zoom));
                     if (points.Count() > 2)
-                    {
-                        g.DrawLines(Pens.Red, points.ToArray());
-                    }
+                        switch (rankId)
+                        {
+                            case 1:
+                                g.DrawLines(Pens.Red, points.ToArray());
+                                break;
+                            case 2:
+                                g.DrawLines(Pens.Yellow, points.ToArray());
+                                break;
+                            case 3:
+                                g.DrawLines(Pens.Wheat, points.ToArray());
+                                break;
+                            case 4:
+                                g.DrawLines(Pens.Gray, points.ToArray());
+                                break;
+                            case 5:
+                                g.DrawLines(Pens.LightGray, points.ToArray());
+                                break;
+                            default:
+                                g.DrawLines(Pens.White, points.ToArray());
+                                break;
+
+                        }
                 }
             }
+
+            //Console.WriteLine(faultCsv.ToString());
+            //File.WriteAllText(@"faultDL-features.csv", faultCsv.ToString());
 
             foreach (var feature in trench.Features)
             {
@@ -56,22 +123,22 @@ namespace Map_FaultDrawTest
                     var points = singleObject.MainPoints.Select(coordinate => new PointF((coordinate.Lon - lonSta) * zoom, (latEnd - coordinate.Lat) * zoom));
                     if (points.Count() > 2)
                     {
-                        g.DrawLines(Pens.Yellow, points.ToArray());
+                        g.DrawLines(Pens.DarkRed, points.ToArray());
                     }
                 }
             }
 
             BackgroundImage = img;
-
+            img.Save("output.png", System.Drawing.Imaging.ImageFormat.Png);
 
         }
 
-        public static Bitmap DrawMap(float latSta, float latEnd, float lonSta, float lonEnd)
+        public static Bitmap DrawMap(float latSta, float latEnd, float lonSta, float lonEnd, float size = 1080f)
         {
 
-            var zoom = 1080f / (latEnd - latSta);
+            var zoom = size / (latEnd - latSta);
 
-            var bitmap = new Bitmap(1920, 1080);
+            var bitmap = new Bitmap((int)(size * 16 / 9), (int)size);
             using var g = Graphics.FromImage(bitmap);
             g.Clear(Color.FromArgb(20, 40, 60));
             using var gp = new GraphicsPath();
